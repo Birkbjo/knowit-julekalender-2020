6fa0fb886ee3111816cf5fee566efc87f55f21a9
Birk Johansson <birk@dhis2.org>
2020-12-15 12:40:16 +0100

    luke 15

--- luke15.js
+++ luke15.js
@@ -9,47 +9,30 @@ const wordPairs = fs
     .split("\n")
     .filter((s) => !!s);
 
-const dictionary = dictionaryList.reduce((acc, curr) => {
-    acc[curr] = 1;
-    return acc;
-}, {});
-
+const dictionary = new Set(dictionaryList);
 const wordPairsTest = `innovasjons, løsheta
 spektral, sikringens
 verdens, spillet`.split("\n");
 
-function solve(words) {
-    const gluewordsSet = new Set();
-    let res = 0;
-
-    words.forEach((w) => {
-        const [w1, w2] = w.split(", ");
-        console.log(w1, w2, w1.concat(w2));
-        for (let i = 0; i < dictionaryList.length; i++) {
-            const dictWord = dictionaryList[i];
-            const potentialGlueword1 = w1.concat(dictWord);
-            const potentialGlueword2 = dictWord.concat(w2);
-
-            //const pot3 = w2.concat(dictWord);
-            //const pot4 = dictWord.concat(w1);
-            // if(dictionary[pot3] && dictionary[pot4])
-            //     console.log('found other potentials', pot3, pot4)
-            if (
-                dictionary[potentialGlueword1] &&
-                dictionary[potentialGlueword2] &&
-                !gluewordsSet.has(dictWord)
-            ) {
-                //console.log("found glueword", dictWord, ': ', potentialGlueword1, ' and ', potentialGlueword2);
-                //gluewords.push(potentialGlueword);
-                res += dictWord.length;
-                gluewordsSet.add(dictWord);
-                break;
-            }
-        }
-    });
-
-    return res;
+function solve(wordPairs) {
+    const glues = wordPairs
+        .map((pair) => pair.split(", "))
+        .flatMap(([w1, w2]) =>
+            dictionaryList
+                .filter((w) => w.startsWith(w1) && w !== w1)
+                .map((gluePart) => gluePart.substring(w1.length))
+                .filter(
+                    (potentialGlue) =>
+                        dictionary.has(potentialGlue + w2) &&
+                        dictionary.has(potentialGlue)
+                )
+        );
+    return Array.from(new Set(glues)).reduce(
+        (acc, curr) => acc + curr.length,
+        0
+    );
 }
+
 console.log("start solve");
 const start = new Date();
 console.log(solve(wordPairs));
